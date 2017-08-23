d0eff2ea8c9c241afa92f692aad3b1dd19a85a6c
alimir71 <alimir71@gmail.com>
2017-08-23 15:28:19 +0430

    Modify grunt updates

---
+++ Gruntfile.js
@@ -0,0 +1,336 @@
+'use strict';
+module.exports = function(grunt) {
+
+    // load all grunt tasks matching the `grunt-*` pattern
+    require('load-grunt-tasks')(grunt);
+    require('time-grunt')(grunt);
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Meta definitions
+        meta: {
+            project:   "wp-ulike",
+            version:   "<%= pkg.title || pkg.name %> - v<%= pkg.version %> (<%= grunt.template.today('yyyy-mm-dd') %>)",
+            copyright: "<%= pkg.author.name %> <%= grunt.template.today('yyyy') %>",
+
+            header: "/*\n" +
+                " *  <%= meta.version %>\n" +
+                " *  <%= pkg.homepage %>\n" +
+                " *\n" +
+                " *  <%= pkg.description %>\n" +
+                " *\n" +
+                " *  <%= meta.copyright %>" +
+                " */\n",
+
+            phpheader: "\n" +
+                " * @package    <%= pkg.name %>\n" +
+                " * @author     <%= pkg.author.name %> <%= grunt.template.today('yyyy') %>\n" +
+                " * @link       <%= pkg.homepage %>",
+
+
+            buildDir: "build",
+            projectSubDir: '<%= meta.project %>',
+            buildPath:     '<%= meta.buildDir %>/<%= meta.projectSubDir %>',
+            installableZipFile: '<%= meta.project %>', // '<%= meta.project %>-installable'
+            zipBuildPath: '<%= meta.buildDir %>/<%= meta.installableZipFile %>.zip'
+        },
+
+        // javascript linting with jshint
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                "force": true
+            },
+
+            gruntFile: {
+                // you can overrides global options for this target here
+                options: {},
+                files: {
+                    src: ['Gruntfile.js']
+                }
+            },
+
+            frontJsScript: {
+                // you can overrides global options for this target here
+                options: {},
+                files: {
+                    src: ['assets/js/wp-ulike.js']
+                }
+            }
+        },
+
+        // Generate POT file
+        makepot: {
+            target: {
+                options: {
+                    domainPath: 'lang',
+                    mainFile: 'wp-ulike.php',
+                    potFilename: 'wp-ulike.pot',
+                    potHeaders: {
+                        poedit: true,
+                        'Report-Msgid-Bugs-To': 'https://wordpress.org/plugins/wp-ulike/',
+                        'Last-Translator': 'Alimir <info@alimir.ir>',
+                        'Language-Team': 'Alimir <info@alimir.ir>',
+                        'x-poedit-keywordslist': '__;_e;__ngettext:1,2;__ngettext_noop:1,2;_n:1,2;_x:1,2c;_nx:4c,1,2;_nx_noop:4c,1,2;_ex:1,2c;esc_attr__;esc_attr_e;esc_attr_x:1,2c;esc_html__;esc_html_e;esc_attr_ex:1,2c;esc_html_x',
+                        'x-poedit-country': 'United States',
+                        'x-textdomain-support': 'yes',
+                    },
+                    type: 'wp-plugin',
+                    updatePoFiles: true
+                }
+            }
+        },
+
+        po2mo: {
+            files: {
+                src: 'lang/*.po',
+                expand: true,
+            },
+        },
+
+        // merge js files
+        concat: {
+
+            frontJsScripts: {
+                options: {
+
+                    banner: "/*! <%= meta.version %>\n" + 
+                        " *  <%= pkg.homepage %>\n" +
+                        " *  <%= meta.copyright %>;\n" +
+                        " */\n",
+
+                    process: function(src, filepath) {
+                        var separator = "\n\n/* ================== " + filepath + " =================== */\n\n\n";
+                        return (separator + src).replace(/;\s*$/, "") + ";"; // make sure always a semicolon is at the end
+                    },
+                },
+                src: [
+                    'assets/js/src/toastr.js',
+                    'assets/js/src/wordpress-ulike.js',
+                    'assets/js/src/scripts.js'
+                ],
+                dest: 'assets/js/wp-ulike.js'
+            }
+
+        },
+
+        // css minify
+        cssmin: {
+            options: {
+                keepSpecialComments: 1
+            },
+            target: {
+                files: {
+                    'assets/css/wp-ulike.min.css': ['assets/css/*.css']
+                }
+            }
+        },     
+
+        clean: {
+            build: [
+                '<%= meta.buildPath %>', '<%= meta.zipBuildPath %>',
+            ],
+            version: [
+                'build/*.txt',
+            ]
+        },
+
+        // JS minification
+        uglify: {
+            options: {
+                compress: {
+                    //drop_console: true
+                },
+                mangle: true,
+                preserveComments: 'some'
+            }, 
+
+            frontJsScripts: {
+                src: '<%= concat.frontJsScripts.dest %>',
+                dest: 'assets/js/wp-ulike.min.js'
+            }         
+        },
+
+        preprocess : {
+            options: {
+                context : {
+                    VERSION: "<%= pkg.version %>",
+                    DEV  : true,
+                    TODO : true,
+                    LITE : false,
+                    PRO  : false,
+                    HEADER: "<%= meta.phpheader %>"
+                }
+            },
+            pro : {
+                src : [ '<%= meta.buildPath %>/**/*.php', '<%= meta.buildPath %>/**/*.css', '<%= meta.buildPath %>/README.txt' ],
+                options: {
+                    inline : true,
+                    context : {
+                        DEV  : false,
+                        TODO : false,
+                        LITE : false,
+                        PRO  : true
+                    }
+                }
+            },
+            liteOfficial : {
+                src : [ '<%= meta.buildPath %>/**/*.php', '<%= meta.buildPath %>/**/*.css', '<%= meta.buildPath %>/README.txt' ],
+                options: {
+                    inline : true,
+                    context : {
+                        DEV  : false,
+                        TODO : false,
+                        LITE : true,
+                        PRO  : false
+                    }
+                }
+            }
+        },
+
+        lineending: {
+            build: {
+                options: {
+                    overwrite: true,
+                    eol: 'lf'
+                },
+                files: {
+                    '': ['<%= meta.buildPath %>/**/*']
+                }
+            }
+        },
+
+        shell:{
+            install:{
+                command: "brew install jpegoptim; brew install pngquant; mkdir <%= meta.buildPath %>"
+            },
+            // Sync package.json version with git repo version
+            updateVersion:{
+                command: 'npm version $(git describe --tags `git rev-list --tags --max-count=1`);'
+            },
+            // Increase package.json version one step
+            bumpVersion: {
+                command: 'npm version patch'
+            },
+
+            cleanBuildDotFiles: {
+                command: ' find <%= meta.buildDir %> -name ".DS_Store" -delete' // exclude dotfiles
+            },
+            zipBuild: {
+                command: 'cd <%= meta.buildDir %>; zip -FSr -9 <%= meta.installableZipFile %> <%= meta.projectSubDir %> -x */\.*; cd ..;' // exclude dotfiles
+            },
+            zipDlPack: {
+                command: 'cd <%= meta.buildDir %>; zip -FSr -9 <%= meta.project %>-download-package * -x /<%= meta.projectSubDir %>/* */\.*; cd ..;' // exclude dotfiles
+            },
+            createTextVersion:{
+                command: 'echo Latest version: v<%= pkg.version %> >> <%= meta.buildDir %>/<%= pkg.version %>.txt'
+            },
+
+            findLitePngs:{
+                command: "find ./<%= meta.buildPath %>/ -name '*.png'"
+            },
+            compressLitePngs:{
+                command: "find ./<%= meta.buildPath %>/ -name '*.png' -exec pngquant --speed 3 --quality=65-80 --skip-if-larger --ext .png --force 256 {}  \\;"
+            },
+            getLitePngsSize:{
+                command: "find ./<%= meta.buildPath %>/ -name '*.png' -exec du -ch {} + | grep total$ "
+            },
+
+            findLiteJpgs:{
+                command: "find ./<%= meta.buildPath %>/ -name '*.jpg'"
+            },
+            compressLiteJpgs:{
+                command: "find ./<%= meta.buildPath %>/ -name '*.jpg' -exec jpegoptim -m80 -o -p  {} \\;"
+            },
+            getLiteJpgsSize:{
+                command: "find ./<%= meta.buildPath %>/ -name '*.jpg' -exec du -ch {} + | grep total$ "
+            }
+        },
+
+        // image optimization
+        imagemin: {
+            dist: {
+                options: {
+                    optimizationLevel: 7,
+                    progressive: true,
+                    interlaced: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'assets/img/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'assets/img/'
+                }]
+            }
+        },        
+
+        // deploy via rsync
+        deploy: {
+            options: {
+                args: ["--verbose --delete-after"], // z:compress while transfering data, P: display progress
+                exclude: [
+                        '.git*', 'node_modules', '.sass-cache', 'Gruntfile.js', 'package.json', 'composer.json',
+                        'assets/js/src', 'assets/js/*.map', 'readme.md', '.jshintrc', 'build', '.*', '.ds_store', 'package-lock.json'
+                ],
+                recursive: true,
+                syncDestIgnoreExcl: true
+            },
+
+            build: {
+                options: {
+                    src: "./",
+                    dest: "<%= meta.buildPath %>"
+                }
+            },
+
+            lite: {
+                options: {
+                    exclude: [
+                        '.git*', 'node_modules', '.sass-cache', 'Gruntfile.js', 'package.json', 'composer.json',
+                        'assets/js/src', 'assets/js/*.map', 'readme.md', '.jshintrc', 'build', '.*', '.ds_store', 'package-lock.json'
+                    ],
+                    src: ['./'],
+                    dest: "<%= meta.buildPath %>"
+                }
+            }
+
+        }
+
+    });
+
+
+    grunt.registerTask( 'install'       , ['shell:install'] );
+    grunt.registerTask( 'compress'      , ['shell:compressLitePngs', 'shell:compressLiteJpgs'] );
+
+    grunt.registerTask( 'buildVersion'  , ['clean:version', 'shell:createTextVersion'] );
+
+    grunt.registerTask( 'towprepo'      , ['deploy:allToLocalRepo'] );
+    grunt.registerTask( 'backup'        , ['towprepo'] );
+
+    grunt.registerTask( 'prepack'       , ['buildVersion']  );
+
+    // rename tasks
+    grunt.renameTask('rsync', 'deploy');
+
+    // register task
+    grunt.registerTask( 'syncversion'   , ['shell:updateVersion'] );
+    grunt.registerTask( 'bump'          , ['shell:bumpVersion'  ] );
+
+    grunt.registerTask( 'i18n'          , [ 'makepot' , 'po2mo' ] );
+
+
+    // compress the product in one pack
+    grunt.registerTask( 'pack'          , ['shell:zipBuild'] );
+
+    // deploy the lite version in /build folder
+    grunt.registerTask( 'beta'           , ['clean:build', 'deploy:lite', 'shell:cleanBuildDotFiles', 'lineending', 'compress'] );
+
+    // build the final lite version in /build folder and pack the product
+    grunt.registerTask( 'build'           , ['concat', 'cssmin','uglify', 'i18n', 'beta', 'preprocess:liteOfficial', 'buildVersion', 'pack'] );
+
+    // register task
+    grunt.registerTask( 'default'       , ['concat','cssmin', 'uglify', 'i18n']);
+
+};
