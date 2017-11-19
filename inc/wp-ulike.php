78ed9bd00b405538e52f68817f1f6a7607907e6c
alimir71 <alimir71@gmail.com>
2017-11-19 14:47:19 +0330

    Transmit AJAX handlers to wp-ulike.php

--- inc/wp-ulike.php
+++ inc/wp-ulike.php
@@ -408,3 +408,6 @@
 
 		wp_send_json($response);
 	}
+	//	wp_ajax hooks for the custom AJAX requests 
+	add_action( 'wp_ajax_wp_ulike_process'			, 'wp_ulike_process' );
+	add_action( 'wp_ajax_nopriv_wp_ulike_process'	, 'wp_ulike_process' );		
