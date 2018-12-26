86c3e9503a7c95ba61dc313f06ab2c1da146e8e7
Ali Mirzaei <alimir71@gmail.com>
2018-12-26 14:04:33 +0330

    Add better transient functions

--- admin/admin-functions.php
+++ admin/admin-functions.php
@@ -125,3 +125,74 @@ function wp_ulike_badge_count_format( $number ){
 		number_format_i18n( $number ) 
 	);
 }
+
+
+/**
+ * Set/update the value of a transient.
+ *
+ * You do not need to serialize values. If the value needs to be serialized, then
+ * it will be serialized before it is set.
+ *
+ *
+ * @param string $transient  Transient name. Expected to not be SQL-escaped. Must be
+ *                           172 characters or fewer in length.
+ * @param mixed  $value      Transient value. Must be serializable if non-scalar.
+ *                           Expected to not be SQL-escaped.
+ * @param int    $expiration Optional. Time until expiration in seconds. Default 0 (no expiration).
+ * @return bool False if value was not set and true if value was set.
+ */
+function wp_ulike_set_transient( $transient, $value, $expiration = 0 ) {
+    global $_wp_using_ext_object_cache;
+
+    $current_using_cache = $_wp_using_ext_object_cache;
+    $_wp_using_ext_object_cache = false;
+
+    $result = set_transient( $transient, $value, $expiration );
+
+    $_wp_using_ext_object_cache = $current_using_cache;
+
+    return $result;
+}
+
+
+/**
+ * Get the value of a transient.
+ *
+ * If the transient does not exist, does not have a value, or has expired,
+ * then the return value will be false.
+ *
+ * @param string $transient Transient name. Expected to not be SQL-escaped.
+ * @return mixed Value of transient.
+ */
+function wp_ulike_get_transient( $transient ) {
+    global $_wp_using_ext_object_cache;
+
+    $current_using_cache = $_wp_using_ext_object_cache;
+    $_wp_using_ext_object_cache = false;
+
+    $result = get_transient( $transient );
+
+    $_wp_using_ext_object_cache = $current_using_cache;
+
+    return $result;
+}
+
+
+/**
+ * Delete a transient.
+ *
+ * @param string $transient Transient name. Expected to not be SQL-escaped.
+ * @return bool true if successful, false otherwise
+ */
+function wp_ulike_delete_transient( $transient ) {
+    global $_wp_using_ext_object_cache;
+
+    $current_using_cache = $_wp_using_ext_object_cache;
+    $_wp_using_ext_object_cache = false;
+
+    $result = delete_transient( $transient );
+
+    $_wp_using_ext_object_cache = $current_using_cache;
+
+    return $result;
+}
