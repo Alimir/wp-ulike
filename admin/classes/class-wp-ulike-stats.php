6be2436cff3b850bf7209aa2a64712b600d35e06
Ali Mirzaei <alimir71@gmail.com>
2018-12-28 13:05:45 +0330

    Add better document for stats class

--- admin/classes/class-wp-ulike-stats.php
+++ admin/classes/class-wp-ulike-stats.php
@@ -91,11 +91,11 @@ if ( ! class_exists( 'wp_ulike_stats' ) ) {
 		}
 
 		/**
-		 * Get posts datasets
+		 * Get posts dataset
 		 *
-		 * @author       	Alimir
-		 * @since           2.0
-		 * @return			JSON Array
+		 * @since 2.0
+		 * @param string $table
+		 * @return void
 		 */
 		public function dataset( $table ){
 			$output  = array();
@@ -117,9 +117,10 @@ if ( ! class_exists( 'wp_ulike_stats' ) ) {
 		/**
 		 * Set custom options for charts
 		 *
-		 * @author       	Alimir
-		 * @since           3.5
-		 * @return			Array
+		 * @since 3.5
+		 * @param string $table
+		 * @param array $options
+		 * @return void
 		 */
 		public function charts( $table, $options = array() ){
 
@@ -171,9 +172,10 @@ if ( ! class_exists( 'wp_ulike_stats' ) ) {
 		/**
 		 * Get The Logs Data From Tables
 		 *
-		 * @author       	Alimir
-		 * @since           2.0
-		 * @return			String
+		 * @author Alimir
+		 * @param string $table
+		 * @since 2.0
+		 * @return String
 		 */
 		public function select_data( $table ){
 
@@ -199,9 +201,9 @@ if ( ! class_exists( 'wp_ulike_stats' ) ) {
 		/**
 		 * Get The Summary Of Like Data
 		 *
-		 * @author       	Alimir
-		 * @since           2.0
-		 * @return			Integer
+		 * @param string $table
+		 * @param string $date
+		 * @return integer
 		 */
 		public function get_data_date( $table, $date ){
 			_deprecated_function( 'get_data_date', '3.5', 'count_logs' );
@@ -211,9 +213,9 @@ if ( ! class_exists( 'wp_ulike_stats' ) ) {
 		/**
 		 * Count all logs from the tables
 		 *
-		 * @author       	Alimir
-		 * @since           3.5
-		 * @return			Integer
+		 * @since 3.5
+		 * @param string $date
+		 * @return integer
 		 */
 		public function count_all_logs( $date = 'all' ){
 			// Result
@@ -229,9 +231,9 @@ if ( ! class_exists( 'wp_ulike_stats' ) ) {
 		/**
 		 * Count logs by table
 		 *
-		 * @author       	Alimir
-		 * @since           3.5
-		 * @return			Integer
+		 * @since 3.5
+		 * @param array $args
+		 * @return void
 		 */
 		public function count_logs( $args = array() ){
 
@@ -276,6 +278,11 @@ if ( ! class_exists( 'wp_ulike_stats' ) ) {
 
 		}
 
+		/**
+		 * Display top likers in html format
+		 *
+		 * @return string
+		 */
 		public function display_top_likers(){
 			$top_likers = $this->get_top_likers();
 			$result     = '';
@@ -317,23 +324,23 @@ if ( ! class_exists( 'wp_ulike_stats' ) ) {
 				SELECT T.user_id, SUM(T.CountUser) AS SumUser, T.ip
 				FROM(
 				SELECT user_id, count(user_id) AS CountUser, ip
-					FROM `%1$sulike`
-					GROUP BY user_id
-					UNION ALL
-					SELECT user_id, count(user_id) AS CountUser, ip
-					FROM `%1$sulike_activities`
-					GROUP BY user_id
-					UNION ALL
-					SELECT user_id, count(user_id) AS CountUser, ip
-					FROM `%1$sulike_comments`
-					GROUP BY user_id
-					UNION ALL
-					SELECT user_id, count(user_id) AS CountUser, ip
-					FROM `%1$sulike_forums`
-					GROUP BY user_id
-					) AS T
-					GROUP BY T.user_id
-					ORDER BY SumUser DESC LIMIT %2$d',
+				FROM `%1$sulike`
+				GROUP BY user_id
+				UNION ALL
+				SELECT user_id, count(user_id) AS CountUser, ip
+				FROM `%1$sulike_activities`
+				GROUP BY user_id
+				UNION ALL
+				SELECT user_id, count(user_id) AS CountUser, ip
+				FROM `%1$sulike_comments`
+				GROUP BY user_id
+				UNION ALL
+				SELECT user_id, count(user_id) AS CountUser, ip
+				FROM `%1$sulike_forums`
+				GROUP BY user_id
+				) AS T
+				GROUP BY T.user_id
+				ORDER BY SumUser DESC LIMIT %2$d',
 				$this->wpdb->prefix,
 				5
 			);
@@ -354,9 +361,9 @@ if ( ! class_exists( 'wp_ulike_stats' ) ) {
 		/**
 		 * Tops Summaries
 		 *
-		 * @author       	Alimir
-		 * @since           3.5
-		 * @return			Array
+		 * @param string $type
+		 * @since 3.5
+		 * @return array
 		 */
 		public function get_top( $type ){
 			switch( $type ){
