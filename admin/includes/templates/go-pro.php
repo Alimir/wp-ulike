c503561f39d669fbc3399a4bdac22d9ada7a1502
Ali <alimir71@gmail.com>
2019-12-16 01:31:19 +0330

    Upgrade go pro page

--- admin/includes/templates/go-pro.php
+++ admin/includes/templates/go-pro.php
@@ -15,9 +15,9 @@
 		<div class="column">
 			<h1><?php echo _e( 'WP ULike Premium', WP_ULIKE_SLUG ); ?></h1>
 			<div class="is-feature">
+			<p>WP ULike Pro is our ultimate solution to cast voting to any type of content you may have on your website. With outstanding and eye-catching widgets, you can have Like and Dislike Button on all of your contents would it be a Post, Comment, BuddyPress Activities, bbPress Topics, WooCommerce products, you name it. Now you can feel your users Love for each part of your content.</p>
+			<p>Besides, this plugin includes detailed logs and analytic reports of all the like and dislike activities on your pages. If you’ like to have complete control and a wide variety of options available to customize your plugins, then WP Ulike Pro is the best choice for you.</p>
 			<?php
-				echo sprintf('<p>%s</p>', __('WP ULike Pro plugin is our ultimate solution to cast voting to any type of content you may have in your website. With outstanding and eye-catching widgets, you can have Like and Dislike Button on all of your contents would it be a Post, Comment, Activities, Forum Topics, WooCommerce products, you name it. Now you can really feel your users Love ❤ for each part of your contents.',WP_ULIKE_SLUG) );
-
 				echo wp_ulike_widget_button_callback( array(
 					'label'         => __( 'Buy WP ULike Premium', WP_ULIKE_SLUG ),
 					'color_name'    => 'default',
@@ -32,6 +32,72 @@
 				) );
 			?>
 			</div>
+		</div>
+	</div>
+	<div class="has-1-columns">
+		<h2 class="is-section-header">Compare WP ULike Free Vs. Pro</h2>
+		<div class="column">
+			<table class="wp-ulike-simple-table table-bordered">
+				<thead>
+					<tr>
+						<th>Features</th>
+						<th>Free</th>
+						<th>PRO</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>The #1 WordPress Voting Plugin</td>
+						<td><img class="wp-ulike-table-icon" src="<?php echo WP_ULIKE_ASSETS_URL; ?>/img/svg/checkmark.svg" alt="Checkmark" /></td>
+						<td><img class="wp-ulike-table-icon" src="<?php echo WP_ULIKE_ASSETS_URL; ?>/img/svg/checkmark.svg" alt="Checkmark" /></td>
+					</tr>
+					<tr>
+						<td>Dislike Votings support</td>
+						<td><img class="wp-ulike-table-icon" src="<?php echo WP_ULIKE_ASSETS_URL; ?>/img/svg/cross-remove.svg" alt="Cross Remove" /></td>
+						<td><img class="wp-ulike-table-icon" src="<?php echo WP_ULIKE_ASSETS_URL; ?>/img/svg/checkmark.svg" alt="Checkmark" /></td>
+					</tr>
+					<tr>
+						<td>+7 Professional Elementor Widgets</td>
+						<td><img class="wp-ulike-table-icon" src="<?php echo WP_ULIKE_ASSETS_URL; ?>/img/svg/cross-remove.svg" alt="Cross Remove" /></td>
+						<td><img class="wp-ulike-table-icon" src="<?php echo WP_ULIKE_ASSETS_URL; ?>/img/svg/checkmark.svg" alt="Checkmark" /></td>
+					</tr>
+					<tr>
+						<td>+8 Carefully Designed Premium Templates</td>
+						<td><img class="wp-ulike-table-icon" src="<?php echo WP_ULIKE_ASSETS_URL; ?>/img/svg/cross-remove.svg" alt="Cross Remove" /></td>
+						<td><img class="wp-ulike-table-icon" src="<?php echo WP_ULIKE_ASSETS_URL; ?>/img/svg/checkmark.svg" alt="Checkmark" /></td>
+					</tr>
+					<tr>
+						<td>Professional Statistics Panel With date range & status controllers</td>
+						<td><img class="wp-ulike-table-icon" src="<?php echo WP_ULIKE_ASSETS_URL; ?>/img/svg/cross-remove.svg" alt="Cross Remove" /></td>
+						<td><img class="wp-ulike-table-icon" src="<?php echo WP_ULIKE_ASSETS_URL; ?>/img/svg/checkmark.svg" alt="Checkmark" /></td>
+					</tr>
+					<tr>
+						<td>Elementor Custom CSS controller for all widgets</td>
+						<td><img class="wp-ulike-table-icon" src="<?php echo WP_ULIKE_ASSETS_URL; ?>/img/svg/cross-remove.svg" alt="Cross Remove" /></td>
+						<td><img class="wp-ulike-table-icon" src="<?php echo WP_ULIKE_ASSETS_URL; ?>/img/svg/checkmark.svg" alt="Checkmark" /></td>
+					</tr>
+					<tr>
+						<td>Complete Range of Options for Settings Panel</td>
+						<td><img class="wp-ulike-table-icon" src="<?php echo WP_ULIKE_ASSETS_URL; ?>/img/svg/cross-remove.svg" alt="Cross Remove" /></td>
+						<td><img class="wp-ulike-table-icon" src="<?php echo WP_ULIKE_ASSETS_URL; ?>/img/svg/checkmark.svg" alt="Checkmark" /></td>
+					</tr>
+						<td>Fast & Complete Supporting Services</td>
+						<td><img class="wp-ulike-table-icon" src="<?php echo WP_ULIKE_ASSETS_URL; ?>/img/svg/cross-remove.svg" alt="Cross Remove" /></td>
+						<td><img class="wp-ulike-table-icon" src="<?php echo WP_ULIKE_ASSETS_URL; ?>/img/svg/checkmark.svg" alt="Checkmark" /></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+	<div class="has-2-columns">
+		<div class="column is-edge-to-edge has-accent-background-color">
+			<img src="<?php echo WP_ULIKE_ASSETS_URL; ?>/img/elementor-support.png" alt="Elementor Support" />
+		</div>
+		<div class="column is-vertically-aligned-center">
+			<h2>Customize Every Part of Your WP ULlike Pro Plugin</h2>
+			<?php
+				echo sprintf('<p>%s</p><p>%s</p>', __('Elementor is the #1 WordPress page builder. In the new version of the WP ULike PRO plugin we fully support this page builder and have a variety of widgets & controllers that make life much easier for you.',WP_ULIKE_SLUG), __('Just drag your desired widget and drop it in your Elementor sections, customize as you go and enjoy your like and dislike buttons on your contents.',WP_ULIKE_SLUG) );
+			?>
 		</div>
 	</div>
 	<div class="has-3-columns">
@@ -71,17 +137,6 @@
 			<h3><?php echo _e( 'Statistics', WP_ULIKE_SLUG ); ?></h3>
 			<?php
 				echo sprintf( '<p>%s</p>', __('extract detailed reports and beautiful, useful and simple charts in an instant.',WP_ULIKE_SLUG) );
-			?>
-		</div>
-	</div>
-	<div class="has-2-columns">
-		<div class="column is-edge-to-edge has-accent-background-color">
-			<img src="<?php echo WP_ULIKE_ASSETS_URL; ?>/img/elementor-support.png" alt="Elementor Support" />
-		</div>
-		<div class="column is-vertically-aligned-center">
-			<h2><?php echo _e( 'Customize Every Part of Your YouLike Plugin', WP_ULIKE_SLUG ); ?></h2>
-			<?php
-				echo sprintf('<p>%s</p><p>%s</p>', __('Elementor is the #1 WordPress page builder. In the new version of the WP ULike PRO plugin we fully support this page builder and have a variety of widgets & controllers that make life much easier for you.',WP_ULIKE_SLUG), __('Just drag your desired widget and drop it in your Elementor sections, customize as you go and enjoy your like and dislike buttons on your contents.',WP_ULIKE_SLUG) );
 			?>
 		</div>
 	</div>
