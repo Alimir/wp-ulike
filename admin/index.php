f6d41d7d86e435575cb77c3769dbae494c9e42e6
Alimir <alimir71@gmail.com>
2020-01-20 16:48:23 +0330

    Call admin panel & pages classes

--- admin/index.php
+++ admin/index.php
@@ -9,10 +9,12 @@ if ( ! defined( 'WPINC' ) ) {
     die('No Naughty Business Please !');
 }
 
+//include settings panel
+require_once( WP_ULIKE_ADMIN_DIR . '/settings/classes/setup.class.php');
+// Register admin menus
+new wp_ulike_admin_panel();
+
 //include about menu functions
 require_once( WP_ULIKE_ADMIN_DIR . '/admin-functions.php');
 //include logs menu functions
 require_once( WP_ULIKE_ADMIN_DIR . '/admin-hooks.php');
-
-// Register admin menus
-new wp_ulike_admin_pages();
