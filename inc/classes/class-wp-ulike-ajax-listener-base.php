cd8ba17a4e7ed5b676a1844295c25eab64a12383
Ali <alimir71@gmail.com>
2020-07-10 00:16:27 +0430

    Add ajax listener base class

---
+++ inc/classes/class-wp-ulike-ajax-listener-base.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+* Base AJAX class
+*/
+abstract class wp_ulike_ajax_listener_base{
+
+	public function __construct(){}
+
+	/**
+	* Send an Error Response
+	* @param $error string
+	*/
+	protected function sendError($error = null){
+		$error = ( $error ) ? $error : __('There was an error processing the request.', WP_ULIKE_SLUG);
+		return wp_send_json_error( $error );
+	}
+
+	/**
+	* Send a response
+	*/
+	protected function response( $response ){
+		return wp_send_json_success( $response );
+	}
+}
