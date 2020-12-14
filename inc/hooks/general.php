e09e22e2b15459b235c96d3e67b120e604cf6eef
Alimir <alimir71@gmail.com>
2020-12-14 23:28:12 +0330

    Disable dev codes

--- inc/hooks/general.php
+++ inc/hooks/general.php
@@ -287,10 +287,10 @@ function wp_ulike_init_plugins_loaded_hook(){
 add_action( 'plugins_loaded', 'wp_ulike_init_plugins_loaded_hook' );
 
 // @if DEV
-function replace_core_jquery_version() {
-    wp_deregister_script( 'jquery' );
-    // Change the URL if you want to load a local copy of jQuery from your own server.
-    wp_register_script( 'jquery', "https://code.jquery.com/jquery-3.5.1.min.js", array(), false );
-}
-add_action( 'wp_enqueue_scripts', 'replace_core_jquery_version' );
+// function replace_core_jquery_version() {
+//     wp_deregister_script( 'jquery' );
+//     // Change the URL if you want to load a local copy of jQuery from your own server.
+//     wp_register_script( 'jquery', "https://code.jquery.com/jquery-3.5.1.min.js", array(), false );
+// }
+// add_action( 'wp_enqueue_scripts', 'replace_core_jquery_version' );
 // @endif
