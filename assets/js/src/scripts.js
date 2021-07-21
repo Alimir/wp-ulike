cc552ef8dafd04e63f8ab73af34182e68e92e942
Ali <alimir71@gmail.com>
2021-07-21 16:59:08 +0430

    Assets compiled

--- assets/js/src/scripts.js
+++ assets/js/src/scripts.js
@@ -1,21 +1,39 @@
 (function ($) {
-  // on document ready
-  $(function () {
-    $(".wpulike").WordpressUlike();
-  });
-
   // Init ulike buttons
   $(".wpulike").WordpressUlike();
 
-  // Update elements on ajax loaded
-  $(document).ajaxComplete(function () {
-    // init WordpressUlike
-    $(".wpulike").WordpressUlike();
-  });
+  /**
+   * jquery detecting div of certain class has been added to DOM
+   */
+  function WordpressUlikeOnElementInserted(
+    containerSelector,
+    elementSelector,
+    callback
+  ) {
+    var onMutationsObserved = function (mutations) {
+      mutations.forEach(function (mutation) {
+        if (mutation.addedNodes.length) {
+          var elements = $(mutation.addedNodes).find(elementSelector);
+          for (var i = 0, len = elements.length; i < len; i++) {
+            callback(elements[i]);
+          }
+        }
+      });
+    };
 
-  // Init on buddypress activity stream
-  $('#buddypress').on('bp_ajax_request', '[data-bp-list="activity"]', function () {
-    $(".wpulike").WordpressUlike();
-  });
+    var target = $(containerSelector)[0];
+    var config = {
+      childList: true,
+      subtree: true,
+    };
+    var MutationObserver =
+      window.MutationObserver || window.WebKitMutationObserver;
+    var observer = new MutationObserver(onMutationsObserved);
+    observer.observe(target, config);
+  }
 
+  // On wp ulike element added
+  WordpressUlikeOnElementInserted("body", ".wpulike", function (element) {
+    $(element).WordpressUlike();
+  });
 })(jQuery);
