d56a20b941177aaca8ddacca836243b5bd91683b
Ali <alimir71@gmail.com>
2024-06-30 11:16:19 +0330

    Fix some issues

--- admin/classes/class-wp-ulike-settings.php
+++ admin/classes/class-wp-ulike-settings.php
@@ -11,6 +11,13 @@ if ( ! defined('ABSPATH') ) {
 
 if ( !class_exists( 'wp_ulike_settings' ) ) {
     class wp_ulike_settings {
+
         public function __construct() {}
+
+        public static function parse_multi( $result ) {
+            // Check if the result was recorded as JSON, and if so, returns an array instead
+            return ( is_string( $result ) && $array = json_decode( $result, true ) ) ? $array : $result;
+        }
+
     }
 }
